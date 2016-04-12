1a4483ae53e0d9cf0d44b977f6daf00c3f561d30
Barbara Maim <barbara@minsh.net>
2016-04-12 15:33:43 +0530

    better youtube

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var minshYoutube = function(args) {
   var youtubeId = args[0];
   if (!youtubeId) { return; }
 
-  return '<div class="video-container"><iframe width="500" height="281" id="youtube_iframe" src="https://www.youtube.com/embed/' + youtubeId + '?feature=oembed&amp;enablejsapi=1&amp;origin=https://safe.txmblr.com&amp;wmode=opaque" frameborder="0"></iframe></div>';
+  return '<div class="video-container"><iframe width="500" height="281" id="youtube_iframe" src="https://www.youtube.com/embed/' + youtubeId + '?feature=oembed&amp;enablejsapi=1&amp;wmode=opaque" frameborder="0" allowfullscreen></iframe></div>';
 };
 
 hexo.extend.tag.register('minsh_youtube', minshYoutube, {async : false});
